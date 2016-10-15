7e6079c955b043c87129d29daffcf9c1780fa0f7
Sunny Ripert <sunny@sunfox.org>
2016-10-15 16:56:39 +0200

    Update Slack plugin

--- lib/meuh/slack_plugin.rb
+++ lib/meuh/slack_plugin.rb
@@ -7,21 +7,35 @@ module Meuh
         @@meuh = Meuh::Brain.new
 
         match /^.*$/ do |client, data, match|
-          channel = client.channels[data.channel]
-          members = channel && channel.members || []
-
-          message = {
-            nickname: client.users[data.user].name,
-            message: match[0],
-            nicknames: members.map { |member| client.users[member].name }
-          }
-
+          message = SlackPlugin.message(client, data, match)
           @@meuh.botname = client.name
           @@meuh.message(message) do |response|
             client.say(channel: data.channel, text: response)
           end
         end
       end
+    end
+
+    def self.message(client, data, match)
+      nickname = client.users[data.user].name
+
+      text = match[0]
+      text = text.gsub("<@#{client.self.id}>", client.name)
+      text = text.gsub(":slightly_smiling_face:", ":)")
+      text = text.gsub(":smile:", ":D")
+      text = text.gsub(":wink:", ";)")
+      text = Slack::Messages::Formatting.unescape(text)
+
+      channel = client.channels[data.channel]
+      members = channel && channel.members || []
+      nicknames = members.map { |member| client.users[member].name }
+
+      puts "-" * 25
+      p(
+        nickname: nickname,
+        message: text,
+        nicknames: nicknames
+      )
     end
   end
 end
