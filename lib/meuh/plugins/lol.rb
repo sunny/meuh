2b2dd27dcbb43a4404fcc4f6bda54bfdce118906
Sunny Ripert <sunny@sunfox.org>
2017-07-03 18:57:23 +0200

    Add `lolilol`

--- lib/meuh/plugins/lol.rb
+++ lib/meuh/plugins/lol.rb
@@ -2,7 +2,7 @@ module Meuh
   module Plugins
     class Lol
       def answer(msg)
-        if msg.text =~ /^(lol|mdr|rofl|ptdr) ?!*$/i
+        if msg.text =~ /^(lol|lolilol|mdr|rofl|ptdr) ?!*$/i
           ['lol', 'mdr', 'rofl', 'ptdr', 'haha'].sample
         end
       end
